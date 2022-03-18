d3966a2c2fb1c9848be35339237944798ca6eb8b
Natalia <nat.lobo.rhyman@gmail.com>
2022-03-18 17:44:44 -0400

    Created a javascript file to separate contents

---
+++ main.js
@@ -0,0 +1,6 @@
+
+const rockPaperScissors = ["Rock", "Paper", "Scissors"];
+    function computerPlay (){
+    let randomPlay = Math.floor(Math.random() * rockPaperScissors.length)
+    console.log(rockPaperScissors[randomPlay])
+    }						    
