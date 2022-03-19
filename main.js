6f474e24c2147e74d71348e2426dd95a48356941
Natalia <nat.lobo.rhyman@gmail.com>
2022-03-19 16:06:40 -0400

    removed function pcCHoice because it was redundant

--- main.js
+++ main.js
@@ -4,21 +4,20 @@
 const rockPaperScissors = ["Rock", "Paper", "Scissors"];
     function computerPlay (){
     let randomPlay = Math.floor(Math.random() * rockPaperScissors.length)
-    return rockPaperScissors[randomPlay]
+    return (rockPaperScissors[randomPlay]).toLowerCase()
     }					  
-console.log(computerPlay())
 
 
-function playerSelection () {
+function playerInput () {
   document.querySelector("#playerChoice").innerText = ""
   return document.querySelector("#playerChoice").value.toLowerCase()
 }
-console.log(playerSelection())
 
-function computerSelection (){
+
+/* function pcChoice (){
   return computerPlay().toLowerCase()
 }
-console.log(computerSelection())
+ */
 
 
 //function playRound plays a single round of Rock Paper Scissors
@@ -27,21 +26,23 @@ document.querySelector('#play').addEventListener('click', playRound)
 
 function playRound(playerSelection, computerSelection) {
         
+         playerSelection = playerInput()
+         computerSelection = computerPlay()
+         
         //return document.querySelector("#computerChoice").innerText
+
+        if (playerSelection === "paper" && computerSelection === "scissors"){
+        return document.querySelector("#result").innerText =  "You Lose! Scissors beats Paper" && document.querySelector("#computerChoice").innerText
         
-        if (playerSelection() === "paper" && computerSelection() === "scissors"){
-        return document.querySelector("#result").innerText =  "You Lose! Scissors beats Paper"
         }
-        else if (playerSelection() === "scissors" && computerSelection() === "rock"){
+        else if (playerSelection === "scissors" && computerSelection === "rock"){
         return document.querySelector("#result").innerText =  "You Lose! Rock beats Scissors"
         }
-        else if (playerSelection() === "rock" && computerSelection() === "paper"){
+        else if (playerSelection === "rock" && computerSelection === "paper"){
         return document.querySelector("#result").innerText =  "You Lose! Paper beats Rock"
         }
-        else if (playerSelection === computerSelection ) {
-            return document.querySelector("#result").innerText =  "It's a tie!"
+        else if (playerSelection === computerSelection) {return document.querySelector("#result").innerText =  "It's a tie!"
         }
-        else {return document.querySelector("p").innerText = "You win!"}
+        else return document.querySelector("#result").innerText = "You win!"
             }
-    
     
