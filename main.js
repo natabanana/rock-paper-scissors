2c8d81d05ceb5276942fc3d6d1d1c759e4e99525
Natalia <nat.lobo.rhyman@gmail.com>
2022-03-21 19:22:25 -0400

    added comments on the functions, deleted function playerInput() & created function game()

--- main.js
+++ main.js
@@ -3,61 +3,68 @@ let playerScoreDisplay = document.createElement('p')
 let computerScoreDisplay = document.createElement('p')
 let resultsDisplay = document.querySelector("#result").innerText
 
-// function called computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
 
-const rockPaperScissors = ["Rock", "Paper", "Scissors"];
-    function computerPlay (){
-    let randomPlay = Math.floor(Math.random() * rockPaperScissors.length)
-    return (rockPaperScissors[randomPlay]).toLowerCase()
+
+
+const playerSelection = document.querySelector(".playerChoice").value.toLowerCase()
+const computerSelection = computerPlay();
+const weaponOptions = ["Rock", "Paper", "Scissors"];
+
+function computerPlay (){
+  //this fucntion will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
+  let randomPlay = Math.floor(Math.random() * weaponOptions.length)
+  // document.querySelector("#computerChoice").innerText
+  return (weaponOptions [randomPlay]).toLowerCase()
     }					  
 
 
-function playerInput () {
-  document.querySelector("#playerChoice").innerText = ""
-  return document.querySelector("#playerChoice").value.toLowerCase()
-}
+document.querySelector('.playerChoice').addEventListener('click', singlePlayRound)
 
 
-//function playRound plays a single round of Rock Paper Scissors
+function singlePlayRound(playerSelection, computerSelection) {
+//function singlePlayRound plays a single round of Rock Paper Scissors
 
-document.querySelector('#play').addEventListener('click', playRound)
+        //return document.querySelector("#computerChoice").innerText
+        if (playerSelection === "paper" && computerSelection === "scissors"){
+        return document.querySelector("#result").innerText =  "You Lose! Scissors beats Paper"}
+          
+        else if (playerSelection === "scissors" && computerSelection === "rock"){
+        return document.querySelector("#result").innerText =  "You Lose! Rock beats Scissors"}
 
+    
+        else if (playerSelection === "rock" && computerSelection === "paper"){
+        return document.querySelector("#result").innerText =  "You Lose! Paper beats Rock"
+          }
+
+        else if (playerSelection === computerSelection ) {
+        return document.querySelector("#result").innerText =  "It's a tie!"}
+
+        else {return document.querySelector("#result").innerText = "You win!"}
+        
+      
+      }
 
 
 let playerScore = 0
 let computerScore = 0 
 
-function playRound(playerSelection, computerSelection) {
-        
-         playerSelection = playerInput()
-         computerSelection = computerPlay()
-         
-        //return document.querySelector("#computerChoice").innerText
+function game() {        
 
-        if (playerSelection === "paper" && computerSelection === "scissors"){
-        computerScore++ 
-        resultsDisplay.textContent = `You lose! ${computerSelection} beats ${playerSelection}`
-        
-        }
-        else if (playerSelection === "scissors" && computerSelection === "rock"){
-        computerScore++ 
-        resultsDisplay.textContent = `You lose! ${computerSelection} beats ${playerSelection}`
-        }
-        else if (playerSelection === "rock" && computerSelection === "paper"){
-        computerScore++ 
-        resultsDisplay.textContent = `You lose! ${computerSelection} beats ${playerSelection}`
-        }
-        else if (playerSelection === computerSelection) {resultsDisplay.textContent=  "It's a tie!"
-        }
-        else {
-        playerScore++
-        resultsDisplay.textContent = `You lose! ${playerSelection} beats ${computerSelection}`}
-            
-      playerScoreDisplay.textContent = "Player" + playerScore
-      computerScoreDisplay.textContent = "Computer" + computerScore
-      
-      }
+resultSingleRound = singlePlayRound(playerSelection, computerSelection)
 
+  for (let i = 0; i < 5; i++) {
+    if (resultSingleRound.split(" ").includes("lose")) {
+      computerScore++
+    }
+    else {
+      playerScore++}
+  }
+  console.log(computerScore)
+  console.log(playerScore)
+  if (playerScore > computerScore){
+    alert("You won!")}
+  
+  else { alert("You lost!")}
 
-
+}
 
