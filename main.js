4de6637bbc30a8fe26d4d33b5905d9178f5f84b7
Natalia <nat.lobo.rhyman@gmail.com>
2022-03-30 18:37:11 -0400

    changed name of function playFiveRounds to playMultipleRounds which now keeps track of the game score
    function game now calls function playMultipleRounds and gives final winner of the game

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ let resultsDisplay = document.querySelector("#result").innerText
 
 const weaponOptions = ["Rock", "Paper", "Scissors"]
 
-function computerPlay (){
+function computerPlay() {
   //this fucntion will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
   let randomPlay = Math.floor(Math.random() * weaponOptions.length)
   // document.querySelector("#computerChoice").innerText
-  return (weaponOptions [randomPlay]).toLowerCase()
-    }					  
+  return (weaponOptions[randomPlay]).toLowerCase()
+}
 
 
 //const playerSelection = document.querySelector("#playerChoice").value.toLowerCase()
@@ -20,60 +20,73 @@ document.querySelector('#play').addEventListener('click', game)
 
 
 function singlePlayRound(playerSelection, computerSelection) {
-//function singlePlayRound plays a single round of Rock Paper Scissors
+  //function singlePlayRound plays a single round of Rock Paper Scissors
 
-       playerSelection = document.querySelector("#playerChoice").value.toLowerCase() 
-       computerSelection = computerPlay()
-        //return document.querySelector("#computerChoice").innerText
-        if ((playerSelection === "paper" && computerSelection === "scissors") ||  (playerSelection === "scissors" && computerSelection === "rock") || playerSelection === "rock" && computerSelection === "paper" ){
-        return `You lose ! ${computerSelection} beats ${playerSelection}`}
-          
-        else if (playerSelection === computerSelection ) {
-        return "It's a tie!"}
+  playerSelection = prompt("Choose rock, paper or scissors ").toLowerCase();
+  computerSelection = computerPlay()
+  //return document.querySelector("#computerChoice").innerText
+  if ((playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock") || playerSelection === "rock" && computerSelection === "paper") {
+    return `You lose ! ${computerSelection} beats ${playerSelection}`
+  }
 
-        else {return `You win ! ${playerSelection} beats ${computerSelection}`  }
-        
-      
-      }
+  else if (playerSelection === computerSelection) {
+    return "It's a tie!"
+  }
+
+  else { return `You win ! ${playerSelection} beats ${computerSelection}` }
+
+
+}
 
 
 let playerScore = 0
-let computerScore = 0 
+let computerScore = 0
 
-function scoreCount() {
-  let singleGame = singlePlayRound() 
-  for (let i = 0; i < 5; i++) {
-    if (singleGame.split(" ").includes("lose")) {
-      computerScore++
-    }
 
-    else if (singleGame.split(" ").includes("win")) {
-      playerScore++
-    }
+
+function playMultipleRounds(numRounds) {
+//function playMultipleRounds plays as many games of Rock,Paper, Scissors are the 
+//user inputs 
+
+  numRounds = prompt("How many rounds would you like to play ")
+  results = []
+  for (let i = 0; i < numRounds; i++) {
+    results[i] = singlePlayRound()
+    if (results[i].split(" ").includes("lose"))
+      {computerScore++}
+
+    else if (results[i].split(" ").includes("win"))
+    {playerScore++}
+
 
   }
-  
+
+  console.log(results)
 }
 
-function game () {
-  let score = scoreCount()
+
+
+function game() {
+  playMultipleRounds()
 
   if (playerScore > computerScore) {
-    console.log( `You win! The computer score is ${computerScore} and your score is ${playerScore}`)
+    console.log(`You win! The computer score is ${computerScore} and your score is ${playerScore}`)
   }
 
-  else {console.log( `You lose! The computer score is ${computerScore} and your score is ${playerScore}`)
+  else {
+    console.log(`You lose! The computer score is ${computerScore} and your score is ${playerScore}`)
+  }
+
+
+
 }
 
 
-}
+function output() {
+  let value = game()
+  const para = document.createElement('p');
+  document.body.appendChild(para);
+  para.textContent = `Result: ${value}`;
 
-
-function output(value) {
-        value = game()
-        const para = document.createElement('p');
-        document.body.appendChild(para);
-        para.textContent = `Result: ${value}`;
-      
 }
 
