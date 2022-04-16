2ea36acbe893f4280abff6bdf47ee78d042c4190
natabanana <nat.lobo.rhyman@gmail.com>
2022-04-16 19:34:28 -0400

    added a div tag to display results and created counter to keep track of score

--- main.js
+++ main.js
@@ -1,16 +1,10 @@
 
-let playerScoreDisplay = document.createElement('p')
-let computerScoreDisplay = document.createElement('p')
-let resultsDisplay = document.querySelector("#result").innerText
-
-
-const weaponOptions = ["Rock", "Paper", "Scissors"]
+const rockPaperScissors = ["Rock", "Paper", "Scissors"]
 
 function computerPlay() {
   //this fucntion will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
-  let randomPlay = Math.floor(Math.random() * weaponOptions.length)
-  // document.querySelector("#computerChoice").innerText
-  return (weaponOptions[randomPlay]).toLowerCase()
+  let randomPlay = Math.floor(Math.random() * rockPaperScissors.length)
+  return (rockPaperScissors[randomPlay]).toLowerCase()
 }
 
 
@@ -21,37 +15,44 @@ function computerPlay() {
 
 
 const buttonOptions = document.querySelectorAll("div.playerSelectionButtons button")
-
 buttonOptions.forEach(button => { button.addEventListener('click', function() {singlePlayRound(button.value)})})
 
+let playerScore = 0
+let computerScore = 0
 
 
 function singlePlayRound(playerSelection, computerSelection) {
   //function singlePlayRound plays a single round of Rock Paper Scissors
   
   computerSelection = computerPlay()
-  //return document.querySelector("#computerChoice").innerText
+
   
+  let result = ""
+  
+  
+
+
   if ((playerSelection === "paper" && computerSelection === "scissors") || (playerSelection === "scissors" && computerSelection === "rock") || playerSelection === "rock" && computerSelection === "paper") {
-    console.log ( `You lose ! ${computerSelection} beats ${playerSelection}`)
+    result =  `<br/> You lose ! ${computerSelection} beats ${playerSelection}. <br/> Player score: ${playerScore}. Computer score: ${computerScore}`
+    computerScore++
   }
 
   else if (playerSelection === computerSelection) {
-   console.log ("It's a tie!")
-  }
+    result = `<br/> It's a tie! <br/> Player score: ${playerScore}. Computer score: ${computerScore}`}
 
-   else { console.log( `You win ! ${playerSelection} beats ${computerSelection}` )}
-
+  else { result =  `<br/> You win ! ${playerSelection} beats ${computerSelection}. <br/> Player score: ${playerScore}. Computer score: ${computerScore}` 
+           playerScore++}
+   
+   document.getElementById("resultsDisplay").innerHTML = result
 
 }
 
 
-let playerScore = 0
-let computerScore = 0
 
 
 
-/* function playMultipleRounds(numRounds) {
+
+ function playMultipleRounds(numRounds) {
 //function playMultipleRounds plays as many games of Rock,Paper, Scissors are the 
 //user inputs 
 
@@ -85,15 +86,6 @@ function game() {
   }
 
 
-
 }
- */
-
-function output() {
-  let value = game()
-  const para = document.createElement('p');
-  document.body.appendChild(para);
-  para.textContent = `Result: ${value}`;
-
-}
+ 
 
